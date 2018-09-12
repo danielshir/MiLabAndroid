69e2dba8134bbce28367fd7f5afe28a2c93fdf49
Daniel Shir <daniels@viber.com>
2018-09-12 16:13:33 +0300

    Added logcat example usage

--- app/src/main/java/com/idc/milab/milabandroid/MainActivity.java
+++ app/src/main/java/com/idc/milab/milabandroid/MainActivity.java
@@ -2,11 +2,25 @@ package com.idc.milab.milabandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+	private static final String TAG = "MainActivity";
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart called");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume called");
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 		pushButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
+				Log.d(TAG, "Pushed button");
 				Toast.makeText(view.getContext(), "Hello World!", Toast.LENGTH_SHORT).show();
 			}
 		});
