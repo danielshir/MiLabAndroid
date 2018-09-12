81c5d7e5cdeda16f0e0c6e09045766fe2be28b79
Daniel Shir <daniels@viber.com>
2018-09-12 16:05:02 +0300

    Push button example

--- app/src/main/java/com/idc/milab/milabandroid/MainActivity.java
+++ app/src/main/java/com/idc/milab/milabandroid/MainActivity.java
@@ -2,6 +2,9 @@ package com.idc.milab.milabandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button pushButton = (Button)findViewById(R.id.button);
+		pushButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(view.getContext(), "Hello World!", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
