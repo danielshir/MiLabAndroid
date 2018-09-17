6413e29f2093e205bc1f5567c91ef94c04ebed40
Daniel Shir <daniels@viber.com>
2018-09-17 15:41:23 +0300

    Example receiving intents with a broadcast receiver

---
+++ app/src/main/java/com/idc/milab/milabandroid/ExampleReceiver.java
@@ -0,0 +1,15 @@
+package com.idc.milab.milabandroid;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ExampleReceiver extends BroadcastReceiver {
+	private static final String TAG = "ExampleReceiver";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Log.i(TAG, "screen is now on");
+	}
+}
--- app/src/main/java/com/idc/milab/milabandroid/MainActivity.java
+++ app/src/main/java/com/idc/milab/milabandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.idc.milab.milabandroid;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.ResultReceiver;
 import android.support.v7.app.AppCompatActivity;
@@ -16,31 +17,13 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 	private static final String TAG = "MainActivity";
 
-	private class ConcatResultReceiver extends ResultReceiver {
-		public ConcatResultReceiver(Handler handler) {
-			super(handler);
-		}
-
-		@Override
-		protected void onReceiveResult(int resultCode, Bundle resultData) {
-			final String result = resultData.getString(ConcatService.EXTRA_RESULT_CONCAT);
-			((EditText)findViewById(R.id.result)).setText(result);
-		}
-	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Button concatButton = (Button)findViewById(R.id.btnConcat);
-		concatButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				String part1 = ((EditText)findViewById(R.id.part1)).getText().toString();
-				String part2 = ((EditText)findViewById(R.id.part2)).getText().toString();
-				ConcatService.doActionConcat(MainActivity.this, new ConcatResultReceiver(new Handler()), part1, part2);
-			}
-		});
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(Intent.ACTION_SCREEN_ON);
+		registerReceiver(new ExampleReceiver(), filter);
 	}
 }
